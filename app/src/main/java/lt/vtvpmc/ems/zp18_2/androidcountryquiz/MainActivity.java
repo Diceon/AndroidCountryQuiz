152e81824bc93d66fa119076bf4ba25ccfd61abb
vardenis pavardenis <google@gmail.com>
2019-03-26 15:24:07 +0200

    Initial commit

---
+++ app/src/main/java/lt/vtvpmc/ems/zp18_2/androidcountryquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package lt.vtvpmc.ems.zp18_2.androidcountryquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/lt/vtvpmc/ems/zp18_2/androidcountryquiz/MainActivityFragment.java
@@ -0,0 +1,6 @@
+package lt.vtvpmc.ems.zp18_2.androidcountryquiz;
+
+import android.app.Fragment;
+
+public class MainActivityFragment extends Fragment {
+}
